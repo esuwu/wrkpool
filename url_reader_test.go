05e733c8d492bb28781af6fcf952d3eecdb99d7a
esuwu <sanya554455@gmail.com>
2021-06-10 15:30:38 +0300

    one more test

--- url_reader_test.go
+++ url_reader_test.go
@@ -18,7 +18,6 @@ func TestReadUrlResultIsNil(t *testing.T) {
 	require.Error(t, err)
 }
 
-
 func TestReadMultiplyUrlResultIsNotNil(t *testing.T) {
 	urlR := NewUrlReader("http://example.com/", "https://www.instagram.com/", "http://example.com/")
 	res, err := urlR.Read()
@@ -45,3 +44,20 @@ func TestReadWithError(t *testing.T) {
 	}
 }
 
+func TestLoadUrlRead(t *testing.T) {
+	var testUrl []string
+	for i := 0; i < 100; i++ {
+		testUrl = append(testUrl, "http://example.com/")
+		testUrl = append(testUrl, "http://instagram.com/")
+	}
+	urlR := NewUrlReader(testUrl...)
+	res, err := urlR.Read()
+	require.NoError(t, err)
+	require.NotNil(t, res)
+
+	require.True(t, len(res) == 200)
+
+	for i := 0; i < 200; i++ {
+		require.NoError(t, res[i].err)
+	}
+}
